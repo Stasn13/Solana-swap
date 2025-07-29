4e0ba2e724c7ea062a8b0ce8cd4c66fb9c5a8398
Stasn13 <stasn139@gmail.com>
2025-07-29 14:05:11 +0200

    add mocks

---
+++ constants/Tokens.ts
@@ -0,0 +1,87 @@
+export const tokens = [
+  {
+    chainId: 101,
+    address: "So11111111111111111111111111111111111111111",
+    symbol: "SOL",
+    name: "Wrapped SOL",
+    decimals: 9,
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
+    extensions: {
+      coingeckoId: "solana",
+    },
+  },
+  {
+    chainId: 101,
+    address: "3SghkPdBSrpF9bzdAy5LwR4nGgFbqNcC6ZSq8vtZdj91",
+    symbol: "EV1",
+    name: "EveryOne Coin",
+    decimals: 9,
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/3SghkPdBSrpF9bzdAy5LwR4nGgFbqNcC6ZSq8vtZdj91/logo.png",
+    tags: ["currency"],
+    extensions: {
+      facebook: "https://facebook.com/everyonecoin",
+      twitter: "https://twitter.com/everyonecoin",
+      website: "https://everyonecoin.com/",
+    },
+  },
+  {
+    chainId: 101,
+    address: "NGK3iHqqQkyRZUj4uhJDQqEyKKcZ7mdawWpqwMffM3s",
+    symbol: "YAKU",
+    name: "Yaku",
+    decimals: 0,
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EK58dp4mxsKwwuySWQW826i3fwcvUK69jPph22VUcd2H/logo.png",
+    tags: ["utility-token"],
+  },
+  {
+    chainId: 101,
+    address: "EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
+    symbol: "USDC",
+    name: "USD Coin",
+    decimals: 6,
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png",
+    tags: ["stablecoin"],
+    extensions: {
+      coingeckoId: "usd-coin",
+      serumV3Usdt: "77quYg4MGneUdjgXCunt9GgM1usmrxKY31twEy3WHwcS",
+      website: "https://www.centre.io/",
+    },
+  },
+  {
+    chainId: 101,
+    address: "Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
+    symbol: "USDT",
+    name: "USDT",
+    decimals: 6,
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB/logo.svg",
+    tags: ["stablecoin"],
+    extensions: {
+      coingeckoId: "tether",
+      serumV3Usdc: "77quYg4MGneUdjgXCunt9GgM1usmrxKY31twEy3WHwcS",
+      website: "https://tether.to/",
+    },
+  },
+  {
+    chainId: 101,
+    address: "FVsXUnbhifqJ4LiXQEbpUtXVdB8T5ADLKqSs5t1oc54F",
+    symbol: "wUSDC_v1",
+    name: "USD Coin (Wormhole v1)",
+    decimals: 6,
+    logoURI:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/FVsXUnbhifqJ4LiXQEbpUtXVdB8T5ADLKqSs5t1oc54F/logo.png",
+    tags: ["wrapped", "wormhole"],
+    extensions: {
+      address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      assetContract:
+        "https://etherscan.io/address/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      bridgeContract:
+        "https://etherscan.io/address/0xf92cD566Ea4864356C5491c177A430C222d7e678",
+      coingeckoId: "usd-coin",
+    },
+  },
+];
