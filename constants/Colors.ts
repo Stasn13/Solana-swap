bdf073b053b200834f4957b5c9f57fa0e4f344a1
Stasn13 <stasn139@gmail.com>
2025-07-30 10:20:40 +0200

    colors update

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -8,16 +8,24 @@ const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
+    accent: '#2AF598',
+    text: '#fdf8f4',
+    lightText: '#d3d3d3',
+    muted: '#888888',
+    background: '#0f0f0f',
+    modalBackground: '#2e2e2e',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
-    background: '#151718',
+    accent: '#2AF598',
+    text: '#b3b3b3',
+    lightText: '#d3d3d3',
+    muted: '#888888',
+    background: '#0f0f0f',
+    modalBackground: '#2e2e2e',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
